c06001343d08963bdb66a417fbd084bd681a3d65
@Siemienik Paweł <siemienik.pawel@gmail.com>
2021-05-19 11:12:23 +0200

    Init with VUE + HOT + HF

---
+++ vue-001/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    {{debug}}
+    <hot-table :settings="hotSettings">
+    </hot-table>
+  </div>
+</template>
+
+<script>
+import { HotTable } from '@handsontable/vue';
+import Handsontable from 'handsontable';
+import { HyperFormula } from 'hyperformula';
+
+export default {
+  name: 'App',
+  data: function() {
+    return {
+      debug:{
+        hotBuildDate: Handsontable.buildDate,
+        hotVersion: Handsontable.version,
+        hfBuildDate: HyperFormula.buildDate,
+        hfVersion: HyperFormula.version,
+      },
+      hotSettings: {
+        data: Handsontable.helper.createSpreadsheetData(10, 10),
+        formulas:{
+          engine: HyperFormula
+        },
+        licenseKey: 'non-commercial-and-evaluation'
+      },
+    }
+  },
+  components: {
+    HotTable
+  }
+}
+</script>
+
+<style src="../node_modules/handsontable/dist/handsontable.full.css"></style>
