935b89fd8d13e9f15ac746f7ec4b62123a82f1a4
Oskar Kowalów <kowalow.oskar@gmail.com>
2021-05-20 11:42:03 +0200

    handsontable example

---
+++ react1/src/stories/HotTable.stories.js
@@ -0,0 +1,17 @@
+import {HotColumn, HotTable} from '@handsontable/react';
+import Handsontable from "handsontable";
+
+export default {
+    title: 'HOT/HotTable',
+    component: HotTable
+};
+
+export const JustATable = (args) => <HotTable {...args} />
+
+
+JustATable.args = {
+    data: Handsontable.helper.createSpreadsheetData(6, 10),
+    colHeaders: true,
+    rowHeaders: true,
+    licenseKey: "non-commercial-and-evaluation",
+}
