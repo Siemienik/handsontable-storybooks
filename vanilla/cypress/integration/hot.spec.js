b1e182db6f5b2ecc9a73ad95a7795952f09975a2
@Siemienik Paweł <siemienik.pawel@gmail.com>
2021-05-21 09:37:00 +0200

    ADD Visual testing for vanilla

--- vanilla/cypress/integration/hot.spec.js
+++ vanilla/cypress/integration/hot.spec.js
@@ -1,20 +1,23 @@
 describe('Hot', () => {
+    // Note the use of `before`
     before(() => {
         cy.visitStorybook()
     })
 
+    // Note the use of `beforeEach`
     beforeEach(() => {
         cy.loadStory('HOT', 'JustATable')
     })
 
     it('Type a formula `=1+2` into A1', () => {
+        cy.get('body').click() // without that does not render HOT. ??!
+
         cy.get('.htCore > tbody')
             .get('tr:first')
             .get('td:first')
             .as('cell')
             .dblclick();
         cy.get('textarea:first').type('{selectall}{del}=1+2{enter}');
-
         cy.matchImageSnapshot();
     })
 })
