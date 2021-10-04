610f03de38a979a1c65d71b08897786140699a91
@Siemienik Paweł <siemienik.pawel@gmail.com>
2021-10-04 18:34:44 +0200

    Add comments configuration

--- vanilla/configurations.js
+++ vanilla/configurations.js
@@ -744,6 +744,24 @@ configurations.ColumnSummary1 = {
         }
     ]
 };
+configurations.Comments = {
+    data: [
+        ['', 'Tesla', 'Nissan', 'Toyota', 'Honda', 'Mazda', 'Ford'],
+        ['2017', 10, 11, 12, 13, 15, 16],
+        ['2018', 10, 11, 12, 13, 15, 16],
+        ['2019', 10, 11, 12, 13, 15, 16],
+    ],
+    rowHeaders: true,
+    colHeaders: true,
+    contextMenu: true,
+    comments: true,
+    cell: [
+        { row: 1, col: 1, comment: { value: 'Some comment' } },
+        { row: 2, col: 2, comment: { value: 'Comment 200x50 px', style: { width: 200, height: 50 } } }
+    ],
+    height: 'auto',
+    licenseKey: 'non-commercial-and-evaluation'
+};
 
 export default configurations
 
