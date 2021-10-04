2fcf1af3e9a7bfd98329dfa8ec6e9984f0e44b5a
@Siemienik Paweł <siemienik.pawel@gmail.com>
2021-10-04 08:56:21 +0200

    Add column sorthing

--- vanilla/configurations.js
+++ vanilla/configurations.js
@@ -681,6 +681,26 @@ configurations.NestedAndCollapsibleColumns =  {
     ],
     licenseKey: 'non-commercial-and-evaluation'
 }
+configurations.ColumnSorting = {
+    data: [
+        ['Tesla', 'Model 3', 'BlueStar', 'USA', '★★★★'],
+        ['Tesla', 'Model S', 'WhiteStar', 'USA', '★★★★★'],
+        ['Mitsubishi', 'iMiEV', '', 'Japan', '★★'],
+        ['Ford', 'Focus EV', '', 'USA', '★★'],
+        ['Mitsubishi', 'iMiEV Sport', '', 'Japan', '★★'],
+        ['Tesla', 'Roadster', 'DarkStar', 'USA', '★★★★★'],
+        ['Volkswagen', 'e-Golf','', 'Germany', '★★'],
+        ['Volkswagen', 'E-Up!', '', 'Germany', '★★'],
+        ['Ford', 'C-Max Energi', '', 'USA', '★'],
+        ['BYD', 'Denza', '', 'China', '★★★'],
+        ['BYD', 'e5', '', 'China', '★★★'],
+        ['BYD', 'e6', '', 'China', '★★★★']
+    ],
+    colHeaders: ['Brand', 'Model', 'Code name', 'Country of origin', 'Rank'],
+    columnSorting: true,
+    height: 'auto',
+    licenseKey: 'non-commercial-and-evaluation'
+}
 
 export default configurations
 
