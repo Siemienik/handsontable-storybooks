d4b82cdcaaeb1953c67e48b961126717df393dcf
@Siemienik Paweł <siemienik.pawel@gmail.com>
2021-10-04 08:36:23 +0200

    Add nested header and collapsible columns

--- vanilla/configurations.js
+++ vanilla/configurations.js
@@ -661,6 +661,26 @@ configurations.CellTypeTime = {
         },
     ]
 };
+configurations.NestedAndCollapsibleColumns =  {
+    data: Handsontable.helper.createSpreadsheetData(5, 10),
+    colHeaders: true,
+    rowHeaders: true,
+    colWidths: 60,
+    height: 'auto',
+    nestedHeaders: [
+        ['A', { label: 'B', colspan: 8 }, 'C'],
+        ['D', { label: 'E', colspan: 4 }, { label: 'F', colspan: 4 }, 'G'],
+        ['H', { label: 'I', colspan: 2 }, { label: 'J', colspan: 2 }, { label: 'K', colspan: 2 }, { label: 'L', colspan: 2 }, 'M'],
+        ['N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W']
+    ],
+    collapsibleColumns: [
+        { row: -4, col: 1, collapsible: true },
+        { row: -3, col: 1, collapsible: true },
+        { row: -2, col: 1, collapsible: true },
+        { row: -2, col: 3, collapsible: true }
+    ],
+    licenseKey: 'non-commercial-and-evaluation'
+}
 
 export default configurations
 
